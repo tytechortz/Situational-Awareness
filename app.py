23ad143280b72e8ae94eed97c47ed60e5c2cd0bb
James Swank <jmswank7@gmail.com>
2023-05-17 09:25:43 -0600

    starts app

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
 from dash import Dash, html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+
+
+
+
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
+
+bgcolor = "#f3f3f1"  # mapbox light map land color
+
+header = html.Div("Arapahoe Situational Awareness", className="h2 p-2 text-white bg-primary text-center")
+
+template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
+
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
+
+app.layout = dbc.Container([
+    header,
+])
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8080)
